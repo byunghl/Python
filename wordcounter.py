fb821b1c8a02a2672853b7e7c7e2021f67836157
byunghl <byunghl@uci.edu>
2013-12-27 05:05:18 -0800

    version 0.1b

--- wordcounter.py
+++ wordcounter.py
@@ -8,21 +8,26 @@ fin = open('book.txt')
 
 dic = {}
 
-def highest():
+#find the most common letter 
+def showhighest():
     key = ""
     highest = dic.values()[0]; #value of 'first' element
     for item in dic:
         if(dic[item] > highest):
             highest = dic[item]
             key = item
-    return "key = " + key + ", value = " + str(highest)
+    print "key = " + key + ", value = " + str(highest)
 
-start = default_timer()
+#for time measuring
+def currentTime():
+    return default_timer()
 
-for line in fin:
-   line = line.rstrip('\n') #remove '\n' on the rightside.
-   temp = line.split(); #return type is list
-   for item in temp:
+#
+def buildDictionary():
+   for line in fin:
+       line = line.rstrip('\n') #remove '\n' on the rightside.
+       temp = line.split(); #return type is list
+       for item in temp:
         item = item.strip()
         item = item.lstrip('\"')
         item = item.rstrip('\"')
@@ -36,13 +41,21 @@ for line in fin:
             dic[item] = dic[item] + 1
         else:
             dic[item] = 1
+#
+def printDictionary():
+    for item in dic:
+        print str(item) + " : " + str(dic.get(item))
+#
+def process():
+    buildDictionary()
+    printDictionary()
 
-for item in dic:
-    print str(item) + " : " + str(dic.get(item))
 
-end = default_timer()
+startTime = currentTime()
+process()
+endTime = currentTime()
 
-print "elapsed time: " + str(end - start)
-print highest()
+print "elapsed time: " + str(endTime - startTime)
+showhighest()
 
 fin.close()
